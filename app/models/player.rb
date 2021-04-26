e2a01e000f5e8c62f62d45b497faede0d84a4f1a
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-04-26 16:59:18 +0200

    Added associations to models

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,7 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :results, dependent: :destroy
+  has_many :trainings, through: :results
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,5 @@
 class Training < ApplicationRecord
   belongs_to :category
+  has_many :results
+  has_many :players, through: :results
 end
