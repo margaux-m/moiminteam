99c8d8cc335f992f17e80da512df080819a6a976
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-05-01 18:22:14 +0200

    Implemented Cloudinary for player avatar

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,7 +6,22 @@ class Player < ApplicationRecord
 
   has_many :results, dependent: :destroy
   has_many :trainings, through: :results
+  has_one_attached :photo
 
   validates :name, :email, :encrypted_password, presence: true
   validates :email, format: { with: Devise.email_regexp }
+
+  after_commit :set_default_avatar, on: [:create]
+
+  private
+
+  def set_default_avatar
+    return if photo.attached?
+
+    photo.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'avatar-default.svg')),
+      filename: 'avatar-default.svg',
+      content_type: 'image/svg'
+    )
+  end
 end
