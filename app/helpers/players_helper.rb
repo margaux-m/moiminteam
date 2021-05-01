f7e08db3e52f44c0d60523e047fc4c9a75a46627
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-05-01 22:23:21 +0200

    Styled player avatar

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,9 +2,9 @@ module PlayersHelper
   def avatar_for(player)
     @avatar = player.photo.key
     if @avatar.empty?
-      image_tag('avatar-default.svg', alt: "#{player.name}'s avatar picture")
+      image_tag('avatar-default.svg', crop: :fit, alt: "#{player.name}'s avatar picture")
     else
-      cl_image_tag(@avatar, alt: "#{player.name}'s avatar picture")
+      cl_image_tag(@avatar, crop: :fit, alt: "#{player.name}'s avatar picture")
     end
   end
 end
