192383eeb2f42ed2f0728929fd15e8a4b5f5c776
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-05-02 19:25:41 +0200

    Added route for dashboard vs home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_player!, only: :home
 
   def home; end
+
+  def dashboard; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  devise_for :players
+
+  unauthenticated do
+    root to: 'pages#home'
+  end
+
+  authenticated do
+    root to: 'pages#dashboard', as: 'dashboard'
+  end
 end
